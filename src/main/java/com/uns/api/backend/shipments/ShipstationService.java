24e856b231ae7a7786ec061bec2b2ed639b734d5
Derek Le <derekle1105@gmail.com>
2024-12-17 15:59:33 -0800

    working on fetching shipstation api

--- src/main/java/com/uns/api/backend/shipments/ShipstationService.java
+++ src/main/java/com/uns/api/backend/shipments/ShipstationService.java
@@ -4,5 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ShipstationService {
-
+    public void getShipments(String date) {
+        final String shipstationURI = "https://ssapi.shipstation.com/shipments?shipDateStart=%s&shipDateEnd=%s";
+    }
 }
